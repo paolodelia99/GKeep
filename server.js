db022a51bd32788072cce757c828ae4d2761d6d5
unknown <paolo.delia99@gmail.com>
2020-01-27 10:58:13 +0100

    Nineth commit (heroku deploy)

--- server.js
+++ server.js
@@ -21,16 +21,16 @@ mongoose.connect(process.env.MONGO_URI,{
 app.use('/api/notes',require('./routes/api/notes'));
 app.use('/api/labels',require('./routes/api/label'));
 
-//Server static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    //Set staic folder
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*',(req,res) =>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
-const port = process.env.PRT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server Started on port ${port}`));
