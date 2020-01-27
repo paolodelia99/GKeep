85f3c30734923d9baf2969aec1a222c6b4294340
unknown <paolo.delia99@gmail.com>
2020-01-27 13:10:27 +0100

    Config commit

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config();
+const connectDB = require('./config/db');
+
 
 const app = express();
 
-//Bodyparser Middleware
-app.use(express.json());
-
 //Connnect to Mongo
-mongoose.connect(process.env.MONGO_URI,{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    })
-    .then(()=> console.log('MongoDb Connected..'))
-    .catch(err => console.log(`DB Connection Error: ${err.message}`));
+connectDB();
+
+//Bodyparser Middleware
+app.use(express.json({ extended: false }));
 
 //Use Routes
 app.use('/api/notes',require('./routes/api/notes'));
 app.use('/api/labels',require('./routes/api/label'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set staic folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.get('*',(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PRT || 5000;
 
 app.listen(port, ()=> console.log(`Server Started on port ${port}`));
