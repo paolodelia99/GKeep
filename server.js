ed48dffa7743185b66b8f2c4e4d5597e10d40220
Paolo <51243932+paolodelia99@users.noreply.github.com>
2020-01-27 13:43:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI,{
     useCreateIndex: true,
     useNewUrlParser: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
     })
     .then(()=> console.log('MongoDb Connected..'))
